f648db9b9142a4e76ae2a4da89802dbdc1be043e
Manny Glover <manny@glovercoding.com>
2021-09-26 17:09:14 -0400

    Modified function to handle both grayscale and color images

--- verify-chessboard-corners/find_chessboard_corners.py
+++ verify-chessboard-corners/find_chessboard_corners.py
@@ -1,52 +1,64 @@
-import numpy as np
 import cv2
+import glob
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-import glob
+import numpy as np
 
-# prepare object points
-#Enter the number of inside corners in x
-nx = 10
-nx = 5
-#Enter the number of inside corners in y
-ny = 7
-ny = 6
+USE_COLOR_IMAGE = True
+SHOW_ORIGINAL_IMAGE = True
+
 # Make a list of calibration images
-#chess_images = glob.glob('./camera_cal/*.jpg')
-chess_images = glob.glob('./stereo_rig/*.png')
+chessboard_images_names = glob.glob('./supporting_files/camera_cal/*.jpg')
+nx = 10
+ny = 7
+#chessboard_images_names = glob.glob('./supporting_files/stereo_rig/*.png')
+#nx = 7
+#ny = 6
 # Select any index to grab an image from the list
-for i in range(len(chess_images)):
+for chessboard_image_name in chessboard_images_names:
     # Read in the image
-    chess_board_image = mpimg.imread(chess_images[i])
-    print('shape of image',chess_board_image.shape)
+    chessboard_image = mpimg.imread(chessboard_image_name)
+    if SHOW_ORIGINAL_IMAGE:
+        cv2.imshow(chessboard_image_name, chessboard_image)
+        cv2.waitKey()
 
-    result_name = chess_images[i]
-    cv2.imshow(result_name, chess_board_image)
-    cv2.waitKey()
-
-    if chess_board_image.shape[2] == 3:
-        color_img = chess_board_image
-        gray_img = cv2.cvtColor(chess_board_image, cv2.COLOR_RGB2GRAY)
+    # Handle RGB and Grayscale images
+    image_is_grayscale = True
+    print ('image shape',chessboard_image.shape)
+    if len(chessboard_image.shape) == 3 and chessboard_image.shape[2] == 3:
+        if USE_COLOR_IMAGE:
+            image = chessboard_image
+            image_is_grayscale = False
+        else:
+            image = cv2.cvtColor(chessboard_image, cv2.COLOR_RGB2GRAY)
+    elif len(chessboard_image.shape) == 2 or chessboard_image.shape[2] == 1:
+        image = chessboard_image
     else:
-        # Convert to color
-        color_img = cv2.cvtColor(chess_board_image, cv2.COLOR_GRAY2RGB)
-        gray_img = chess_board_image
+        print ('Unhandled image',chessboard_image_name,' unhandled shape',chessboard_image.shape)
+        exit(1)
+
     # Find the chessboard corners
-    ret, corners = cv2.findChessboardCorners(gray_img, (nx, ny), None)
-    print('corners shape')
-    print(corners.shape)
+    ret, corners = cv2.findChessboardCorners(image, (nx, ny), None)
     corners = np.squeeze(corners)
-    print(corners.shape)
+    
     # If found, draw corners
     if ret == True:
-        # Draw and display the corners
-        #cv2.drawChessboardCorners(chess_board_image, (nx, ny), corners, ret)
-        cv2.drawChessboardCorners(color_img, (nx, ny), corners, ret)
-        for w in range(0,len(corners)):
-            color_img = cv2.circle(color_img, (corners[w][0], corners[w][1]), radius=24,color=(0,0,255), thickness=6)
-        result_name = 'board'+str(i)+'.jpg'
-        #cv2.imshow(result_name, chess_board_image)
-        cv2.imshow(result_name, color_img)
+        # Convert image to color for display
+        if image_is_grayscale:
+            display_image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
+        else:
+            display_image = image
+
+        # Draw image and display the corners
+        cv2.drawChessboardCorners(display_image, (nx, ny), corners, ret)
+
+        if False:
+            # Draw larger circles at chessboard corners for clarity
+            for w in range(0,len(corners)):
+                color_img = cv2.circle(image, (corners[w][0], corners[w][1]), radius=24,color=(0,0,255), thickness=6)
+            result_name = chessboard_image_name
+
+        cv2.imshow(chessboard_image_name, display_image)
         cv2.waitKey()
     else:
-        print ('return is false')
+        print ('Unable to find chessboard corners for image',chessboard_image_name)
