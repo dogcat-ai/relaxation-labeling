44202d5361f9a8a1b5ffab39e0e3c70d03590b15
Manny Glover <manny@glovercoding.com>
2021-10-08 21:42:41 -0400

    Stuck for now: corners is coming over empty.

--- verify-chessboard-corners/verify_chessboard_corners.py
+++ verify-chessboard-corners/verify_chessboard_corners.py
@@ -12,29 +12,30 @@ corePath = os.path.realpath(corePath)
 sys.path.append(corePath)
 from relax import RelaxationLabeling
 
-
 class VerifyChessboardCorners(RelaxationLabeling):
     def __init__(self):
         self.debugTabs = dt.DebugTabs()
         self.findChessboardCorners = FindChessboardCorners()
+        self.debugTabs.print("self.findChessboardCorners.corners: {}".format(self.findChessboardCorners.corners))
+        exit(1)
         self.numObjects = self.findChessboardCorners.nx*self.findChessboardCorners.ny
         self.numLabels = 2 # "good" or "bad"
         self.compatType = 3 # 1: i,j; 2: k,l; 3: m,n
         self.save = True
+        self.initCornerObjectsAndGoodBadLabels()
         super(VerifyChessboardCorners, self).__init__(self.numObjects, self.numLabels, self.compatType, self.save)
 
     def initCornerObjectsAndGoodBadLabels(self):
         self.labels = [0, 1] # 0: default, "good"; 1: special case, "bad"
         self.objects = [[]] # TODO|MEG|OPTIMIZATION|SPEED: Consider turning into a NumPy array
         self.debugTabs.print("nx: {} ny: {}".format(self.findChessboardCorners.nx, self.findChessboardCorners.ny))
+        self.debugTabs.print("corners: {}".format(self.findChessboardCorners.corners))
         for c, corner in enumerate(self.findChessboardCorners.corners):
             if c % self.findChessboardCorners.nx and c == 0:
                 self.objects.append([])
             self.objects[-1].append(corner)
             self.debugTabs.print("c: {} corner: {}".format(c,corner))
             self.debugTabs.print("objects: {}".format(self.objects))
-
-
 
     def calculateCompatibility(self):
         if self.compatType == 2:
@@ -52,11 +53,10 @@ class VerifyChessboardCorners(RelaxationLabeling):
                         for m in range(self.numObjects):
                             for n in range(self.numLabels):
                                     #self.compatibility[i,j,k,l,m,n] = 
-                                pass
+                                    self.debugTabs.print("calculate3DCompatibility not implemented yet.  Exiting...")
+                                    exit(1)
 
     def main(self):
-        self.initCornerObjectsAndGoodBadLabels()
-
         super(VerifyChessboardCorners, self).main()
 
 verifyChessboardCorners = VerifyChessboardCorners()
